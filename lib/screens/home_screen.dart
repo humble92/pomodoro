6a78b7f036050aa91b4006a7bad508eb490dfc61
Daniel Hwang <humble92@gmail.com>
2023-09-28 18:15:34 -0400

    ADD: round & goal

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,18 +10,36 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  static const twentyFiveMinutes = 1500;
-  int totalSeconds = twentyFiveMinutes;
-  bool isRunning = false;
+  static const sixtySeconds = 60;
+  final String title = "PomoTimer";
+  final int roundUnits = 4;
+
+  int _minutesDefined = 25;
+  int totalSeconds = 0;
   int totalPomodoros = 0;
+  int subPomodoros = 0;
+
+  bool isRunning = false;
+  bool isEnable = true;
   late Timer timer;
+
+  @override
+  void initState() {
+    totalSeconds = _minutesDefined * sixtySeconds;
+    super.initState();
+  }
 
   void onTick(Timer timer) {
     if (totalSeconds == 0) {
       setState(() {
-        totalPomodoros = totalPomodoros + 1;
+        subPomodoros++;
+        if (subPomodoros == roundUnits) {
+          totalPomodoros = totalPomodoros + 1;
+          subPomodoros = 0;
+          _reEnable();
+        }
         isRunning = false;
-        totalSeconds = twentyFiveMinutes;
+        totalSeconds = _minutesDefined * sixtySeconds;
       });
       timer.cancel();
     } else {
@@ -32,6 +50,8 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void onStartPressed() {
+    if (!isEnable) return;
+
     timer = Timer.periodic(
       const Duration(seconds: 1),
       onTick,
@@ -50,7 +70,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void onResetPressed() {
     setState(() {
-      totalSeconds = twentyFiveMinutes;
+      totalSeconds = _minutesDefined * sixtySeconds;
     });
   }
 
@@ -63,6 +83,15 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(title),
+      ),
       body: Column(
         children: [
           Flexible(
@@ -84,14 +113,29 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                IconButton(
-                  iconSize: 120,
-                  color: Theme.of(context).cardColor,
-                  onPressed: isRunning ? onPausePressed : onStartPressed,
-                  icon: Icon(isRunning
-                      ? Icons.pause_circle_outline
-                      : Icons.play_circle_outline),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _buildCountdownSquare(1),
+                      for (int i = 15; i <= 35; i += 5)
+                        _buildCountdownSquare(i),
+                    ],
+                  ),
                 ),
+                isEnable
+                    ? IconButton(
+                        iconSize: 120,
+                        color: Theme.of(context).cardColor,
+                        onPressed: isRunning ? onPausePressed : onStartPressed,
+                        icon: Icon(isRunning
+                            ? Icons.pause_circle_outline
+                            : Icons.play_circle_outline),
+                      )
+                    : IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.play_arrow_outlined)),
                 IconButton(
                   iconSize: 30,
                   color: Theme.of(context).cardColor,
@@ -108,7 +152,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 Expanded(
                   child: Container(
                     decoration: BoxDecoration(
-                      color: Theme.of(context).cardColor,
+                      color: Theme.of(context).primaryColor.withOpacity(0.68),
                       borderRadius: const BorderRadius.only(
                         topLeft: Radius.circular(50),
                         topRight: Radius.circular(50),
@@ -116,27 +160,57 @@ class _HomeScreenState extends State<HomeScreen> {
                         bottomRight: Radius.circular(9),
                       ),
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        Text(
-                          'Pomodoros',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                            color:
-                                Theme.of(context).textTheme.displayLarge!.color,
-                          ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'ROUND',
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              '$subPomodoros/$roundUnits',
+                              style: TextStyle(
+                                fontSize: 58,
+                                fontWeight: FontWeight.w600,
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .displayLarge!
+                                    .color,
+                              ),
+                            ),
+                          ],
                         ),
-                        Text(
-                          '$totalPomodoros',
-                          style: TextStyle(
-                            fontSize: 58,
-                            fontWeight: FontWeight.w600,
-                            color:
-                                Theme.of(context).textTheme.displayLarge!.color,
-                          ),
-                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'GOAL',
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              '$totalPomodoros/12',
+                              style: TextStyle(
+                                fontSize: 58,
+                                fontWeight: FontWeight.w600,
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .displayLarge!
+                                    .color,
+                              ),
+                            ),
+                          ],
+                        )
                       ],
                     ),
                   ),
@@ -147,5 +221,47 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
     );
+  }
+
+  Widget _buildCountdownSquare(int number) {
+    return Container(
+      width: 100,
+      height: 60,
+      decoration: BoxDecoration(
+        color: number == _minutesDefined
+            ? const Color.fromARGB(255, 238, 176, 41)
+            : Colors.white,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Center(
+        child: TextButton(
+          onPressed: () {
+            setState(() {
+              _minutesDefined = number;
+              totalSeconds = _minutesDefined * sixtySeconds;
+            });
+          },
+          child: Text(
+            number.toString(),
+            style: const TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _reEnable() {
+    isEnable = false;
+    Timer.periodic(const Duration(seconds: 1 * sixtySeconds), (timer) {
+      setState(() {
+        _minutesDefined--;
+      });
+
+      if (_minutesDefined <= 0) {
+        timer.cancel();
+        isEnable = true;
+        setState(() {});
+      }
+    });
   }
 }
